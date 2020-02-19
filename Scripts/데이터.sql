ef7bc3d58325b0b2685d93ce9839e08280cc06c4
bus503 <bus503@daum.net>
2020-02-19 16:59:21 +0900

    초기작업 마지막 커밋

---
+++ Scripts/데이터.sql
@@ -0,0 +1,29 @@
+select user(), database ();
+
+-- member 회원
+desc member;
+insert into member(mber_id, mber_pass, mber_name, mber_brthdy, mber_adres, mber_tel, total_le_cnt, lend_book_cnt, grade, lend_psb_cdt, join_dt, wdr_cdt) values
+();
+
+-- grade 회원등급
+
+-- request_book 신청도서
+ 
+-- lending 대여/반납
+
+-- book 도서
+
+-- publishing_company 출판사
+
+-- recommendation 추천도서
+
+-- large_classification 대분류
+
+-- middle_classification 중분류
+
+-- librarian 사서
+
+-- title 직책
+
+-- zip_code 우편번호
+

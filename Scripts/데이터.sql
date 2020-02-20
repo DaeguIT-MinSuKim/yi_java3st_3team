21b242a77c6f7d42249df0baec88848891a9a8bc
yoogj0513 <yoogj0513@naver.com>
2020-02-21 00:51:46 +0900

    member 파일 충돌 내용 수정

--- Scripts/데이터.sql
+++ Scripts/데이터.sql
@@ -1,17 +1,17 @@
 select user(), database ();
 
--- member 회원
-desc member;
-load data local infile 'D:/workspace/workspace-teamProject/yi_java3st_3team/document/data/member.txt'
-into table yi_java3st_3team.member 
-character set 'utf8'
-fields terminated by ',';
-
 -- grade 회원등급
 desc grade;
 insert into grade values
 (1,'일반',5),
 (2,'우수',7);
+
+-- member 회원
+desc member;
+load data local infile 'D:/workspace/workspace_teamProject/yi_java3st_3team/document/data/member.txt'
+into table yi_java3st_3team.member
+character set 'utf8'
+fields terminated by ',';
 
 -- request_book 신청도서
 desc request_book; 
@@ -20,11 +20,13 @@ desc request_book;
 -- lending 대여/반납
 
 -- book 도서
+desc book;
+
 
 -- publishing_company 출판사
 desc publishing_company;
-load data local infile 'D:/workspace/workspace-teamProject/yi_java3st_3team/document/data/pls_data.csv'
-into table yi_java3st_3team.librarian 
+load data local infile 'D:/workspace/workspace_teamProject/yi_java3st_3team/document/data/pls_data.csv'
+into table yi_java3st_3team.publishing_company
 character set 'utf8'
 fields terminated by ','
 ignore 1 lines;
@@ -35,13 +37,6 @@ ignore 1 lines;
 
 -- middle_classification 중분류
 
--- librarian 사서
-desc librarian;
-load data local infile 'D:/workspace/workspace-teamProject/yi_java3st_3team/document/data/librarian.txt'
-into table yi_java3st_3team.librarian 
-character set 'utf8'
-fields terminated by ',';
-
 -- title 직책
 desc title;
 insert into title values
@@ -49,6 +44,12 @@ insert into title values
 (2,'사서'),
 (3,'회원');
 
+-- librarian 사서
+desc librarian;
+load data local infile 'D:/workspace/workspace_teamProject/yi_java3st_3team/document/data/librarian.txt'
+into table yi_java3st_3team.librarian 
+character set 'utf8'
+fields terminated by ',';
 
 -- zip_code 우편번호
 

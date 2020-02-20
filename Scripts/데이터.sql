9e88372ea2ba27e9701a1936ee50c2b56f0c37a1
yoogj0513 <yoogj0513@naver.com>
2020-02-21 01:42:12 +0900

    도서 데이터, 대분류 데이터 insert

--- Scripts/데이터.sql
+++ Scripts/데이터.sql
@@ -8,7 +8,7 @@ insert into grade values
 
 -- member 회원
 desc member;
-load data local infile 'D:/workspace/workspace_teamProject/yi_java3st_3team/document/data/member.txt'
+load data local infile 'I:/eclipse-workspace-teamProject/yi_java3st_3team/document/data/member.txt'
 into table yi_java3st_3team.member
 character set 'utf8'
 fields terminated by ',';
@@ -16,16 +16,19 @@ fields terminated by ',';
 -- request_book 신청도서
 desc request_book; 
 
- 
 -- lending 대여/반납
 
 -- book 도서
 desc book;
-
+load data local infile 'I:/eclipse-workspace-teamProject/yi_java3st_3team/document/data/book_data.csv'
+into table yi_java3st_3team.book 
+character set 'utf8'
+fields terminated by ','
+ignore 1 lines;
 
 -- publishing_company 출판사
 desc publishing_company;
-load data local infile 'D:/workspace/workspace_teamProject/yi_java3st_3team/document/data/pls_data.csv'
+load data local infile 'I:/eclipse-workspace-teamProject/yi_java3st_3team/document/data/pls_data.csv'
 into table yi_java3st_3team.publishing_company
 character set 'utf8'
 fields terminated by ','
@@ -34,8 +37,23 @@ ignore 1 lines;
 -- recommendation 추천도서
 
 -- large_classification 대분류
+desc large_classification;
+insert into large_classification values
+(01, '지식학문'),
+(02, '철학'),
+(03, '종교'),
+(04, '사회과학'),
+(05, '자연과학'),
+(06, '기술과학'),
+(07, '예술'),
+(08, '언어'),
+(09, '문학'),
+(10, '역사');
 
 -- middle_classification 중분류
+desc middle_classification ;
+insert into middle_classification values
+(01, '신문', 01)
 
 -- title 직책
 desc title;
@@ -46,7 +64,7 @@ insert into title values
 
 -- librarian 사서
 desc librarian;
-load data local infile 'D:/workspace/workspace_teamProject/yi_java3st_3team/document/data/librarian.txt'
+load data local infile 'I:/eclipse-workspace-teamProject/yi_java3st_3team/document/data/librarian.txt'
 into table yi_java3st_3team.librarian 
 character set 'utf8'
 fields terminated by ',';
--- Scripts/테스트.sql
+++ Scripts/테스트.sql
@@ -7,11 +7,17 @@ from librarian;
 select *
 from member;
 
-
 select *
 from grade;
-
 
 select * 
 from publishing_company;
 
+select *
+from book;
+
+select *
+from large_classification;
+
+select *
+from middle_classification;

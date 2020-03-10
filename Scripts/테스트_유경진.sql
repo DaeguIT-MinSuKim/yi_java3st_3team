023e0d03d9a0dbfabe51bbef8f776932b1584573
yoogj0513 <yoogj0513@naver.com>
2020-03-11 03:26:02 +0900

    보유도서 리스트 테이블 완성, 도서 검색 작업중

--- Scripts/테스트_유경진.sql
+++ Scripts/테스트_유경진.sql
@@ -69,50 +69,52 @@ select book_name, count(*) as cnt
 
 -- 도서검색
 -- all 
-select b1.book_code , b1.book_name, b1.authr_name , b1.trnslr_name , b1.pls , b1.pblicte_year ,
-	   b1.book_price , b2.book_cnt, b1.lend_psb_cdt , b1.total_le_cnt , b1.book_img , b1.lc_no, b1.ml_no ,
+select b1.book_code , b1.book_name, b1.authr_name , b1.trnslr_name , b1.pls, p.pls_name , b1.pblicte_year ,
+	   b1.book_price , b2.book_cnt, b1.lend_psb_cdt , b1.total_le_cnt , b1.book_img , b1.lc_no, l.lclas_name , b1.ml_no, m.mlsfc_name ,
 	   b1.regist_date , b1.dsuse_cdt
-	from book b1, 
-		(select book_name, authr_name , pls, pblicte_year , book_price , count(*) as book_cnt
-			from book
-			group by book_name, authr_name , pls, pblicte_year , book_price) b2
-	where b1.book_name = b2.book_name and b1.authr_name = b2.authr_name and b1.pls = b2.pls and b1.pblicte_year = b2.pblicte_year and 
-			b1.book_price = b2.book_price
+	from book b1 
+		left join publishing_company p on b1.pls = p.pls_no left join large_classification l on b1.lc_no = l.lclas_no 
+		left join middle_classification m on m.mlsfc_no = b1.ml_no and l.lclas_no = m.lclas_no,
+		(select book_name, authr_name , pls, pblicte_year , book_price , count(*) as book_cnt from book group by book_name, authr_name , pls, pblicte_year , book_price) b2
+	where b1.book_name = b2.book_name and b1.authr_name = b2.authr_name and b1.pls = b2.pls and b1.pblicte_year = b2.pblicte_year and b1.book_price = b2.book_price
 	order by b1.regist_date;
 
 -- 도서코드 검색
-select b1.book_code , b1.book_name, b1.authr_name , b1.trnslr_name , b1.pls , b1.pblicte_year ,
-	   b1.book_price , b2.book_cnt, b1.lend_psb_cdt , b1.total_le_cnt , b1.book_img , b1.lc_no, b1.ml_no ,
+select b1.book_code , b1.book_name, b1.authr_name , b1.trnslr_name , b1.pls, p.pls_name , b1.pblicte_year ,
+	   b1.book_price , b2.book_cnt, b1.lend_psb_cdt , b1.total_le_cnt , b1.book_img , b1.lc_no, l.lclas_name , b1.ml_no,
 	   b1.regist_date , b1.dsuse_cdt
-	from book b1, 
-		(select book_name, authr_name , pls, pblicte_year , book_price , count(*) as book_cnt
-			from book
-			group by book_name, authr_name , pls, pblicte_year , book_price) b2
+	from book b1 
+		left join publishing_company p on b1.pls = p.pls_no left join large_classification l on b1.lc_no = l.lclas_no 
+		left join middle_classification m on m.mlsfc_no = b1.ml_no and l.lclas_no = m.lclas_no,
+		(select book_name, authr_name , pls, pblicte_year , book_price , count(*) as book_cnt from book group by book_name, authr_name , pls, pblicte_year , book_price) b2
 	where b1.book_name = b2.book_name and b1.authr_name = b2.authr_name and b1.pls = b2.pls and b1.pblicte_year = b2.pblicte_year and 
-			b1.book_price = b2.book_price and b1.book_code like 'D%' and b1.lc_no = 9;
+			b1.book_price = b2.book_price and b1.book_code like 'D%' and b1.lc_no = 9
+	order by b1.regist_date;
 		
 -- 도서명 검색
-select b1.book_code , b1.book_name, b1.authr_name , b1.trnslr_name , b1.pls , b1.pblicte_year ,
-	   b1.book_price , b2.book_cnt, b1.lend_psb_cdt , b1.total_le_cnt , b1.book_img , b1.lc_no, b1.ml_no ,
+select b1.book_code , b1.book_name, b1.authr_name , b1.trnslr_name , b1.pls, p.pls_name , b1.pblicte_year ,
+	   b1.book_price , b2.book_cnt, b1.lend_psb_cdt , b1.total_le_cnt , b1.book_img , b1.lc_no, l.lclas_name , b1.ml_no,
 	   b1.regist_date , b1.dsuse_cdt
-	from book b1, 
-		(select book_name, authr_name , pls, pblicte_year , book_price , count(*) as book_cnt
-			from book
-			group by book_name, authr_name , pls, pblicte_year , book_price) b2
+	from book b1 
+		left join publishing_company p on b1.pls = p.pls_no left join large_classification l on b1.lc_no = l.lclas_no 
+		left join middle_classification m on m.mlsfc_no = b1.ml_no and l.lclas_no = m.lclas_no,
+		(select book_name, authr_name , pls, pblicte_year , book_price , count(*) as book_cnt from book group by book_name, authr_name , pls, pblicte_year , book_price) b2
 	where b1.book_name = b2.book_name and b1.authr_name = b2.authr_name and b1.pls = b2.pls and b1.pblicte_year = b2.pblicte_year and 
-			b1.book_price = b2.book_price and b1.book_name like '%북%' and b1.lc_no = 6;
+			b1.book_price = b2.book_price and b1.book_name like '%북%' and b1.lc_no = 6
+	order by b1.regist_date;
 
 		
 -- 도서검색 (회원)
-select b1.book_name, b1.authr_name , b1.trnslr_name , b1.pls , b1.pblicte_year ,
-	   b1.book_price , b2.book_cnt, b1.lend_psb_cdt , b1.total_le_cnt , b1.book_img , b1.lc_no, b1.ml_no ,
+select b1.book_code , b1.book_name, b1.authr_name , b1.trnslr_name , b1.pls, p.pls_name , b1.pblicte_year ,
+	   b1.book_price , b2.book_cnt, b1.lend_psb_cdt , b1.total_le_cnt , b1.book_img , b1.lc_no, l.lclas_name , b1.ml_no,
 	   b1.regist_date , b1.dsuse_cdt
-	from book b1, 
-		(select book_name, authr_name , pls, pblicte_year , book_price , count(*) as book_cnt
-			from book
-			group by book_name, authr_name , pls, pblicte_year , book_price) b2
+	from book b1 
+		left join publishing_company p on b1.pls = p.pls_no left join large_classification l on b1.lc_no = l.lclas_no 
+		left join middle_classification m on m.mlsfc_no = b1.ml_no and l.lclas_no = m.lclas_no,
+		(select book_name, authr_name , pls, pblicte_year , book_price , count(*) as book_cnt from book group by book_name, authr_name , pls, pblicte_year , book_price) b2
 	where b1.book_name = b2.book_name and b1.authr_name = b2.authr_name and b1.pls = b2.pls and b1.pblicte_year = b2.pblicte_year and 
-			b1.book_price = b2.book_price and b1.book_name like '%북유럽%' and b1.authr_name like '김%' and b1.lc_no = 10;
+			b1.book_price = b2.book_price and b1.book_name like '%북유럽%' and b1.authr_name like '김%' and b1.lc_no = 10
+	order by b1.regist_date;
 
 
 select * from book;
@@ -133,7 +135,7 @@ update book
 		
 delete 
 	from book 
-	where book_code = 'D010104';
+	where book_code = 'D030104';
 		 
 
 -- 출판사
@@ -236,6 +238,12 @@ alter table request_book auto_increment = 11;
 
 
 
-
+select b1.book_code , b1.book_name, b1.authr_name , b1.trnslr_name , b1.pls, p.pls_name , b1.pblicte_year , 
+	   b1.book_price , b2.book_cnt, b1.lend_psb_cdt , b1.total_le_cnt , b1.book_img , b1.lc_no, l.lclas_name , b1.ml_no, 
+	   b1.regist_date , b1.dsuse_cdt 
+	from book b1 
+		left join publishing_company p on b1.pls = p.pls_no left join large_classification l on b1.lc_no = l.lclas_no 
+		left join middle_classification m on m.mlsfc_no = b1.ml_no and l.lclas_no = m.lclas_no,
+		(select book_name, authr_name , pls, pblicte_year , book_price , count(*) as book_cnt from book group by book_name, authr_name , pls, pblicte_year , book_price) b2 	where b1.book_name = b2.book_name and b1.authr_name = b2.authr_name and b1.pls = b2.pls and b1.pblicte_year = b2.pblicte_year and b1.book_price = b2.book_price and b1.book_code like '%D%' and b1.lc_no = 9  order by b1.regist_date
 
 
---
+++ src/main/java/yi_java3st_3team/ui/list/BookTblPanel.java
@@ -0,0 +1,99 @@
+package yi_java3st_3team.ui.list;
+
+import javax.swing.SwingConstants;
+
+import yi_java3st_3team.dto.Book;
+
+@SuppressWarnings("serial")
+public class BookTblPanel extends AbsListPanel<Book> {
+
+	@Override
+	protected void setTblWidthAlign() {
+		tableSetWidth(100, 150, 100, 100, 100, 70, 50, 80, 50, 50, 100, 100, 50);
+		tableCellAlign(SwingConstants.CENTER, 0, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12);
+		tableCellAlign(SwingConstants.RIGHT, 5);
+	}
+
+	@Override
+	protected String[] getColNames() {
+		return new String[] {
+				"도서코드", "도서명", "저자/역자", "출판사", "발행년도", "도서가격", "보유권수", "대여가능여부", "총대여횟수", "도서이미지", "분류", "등록일자", "폐기여부"
+		};
+	}
+
+	@Override
+	protected Object[] toArray(Book item) {
+		String writer;
+		String lendCdt;
+		String imgCdt = item.getBookImg().length != 8706 && item.getBookImg().length > 0? "있음" : "없음";
+		String dsuseCdt = item.getDsuseCdt() == 0 ? "No" : "Yes";
+		
+		if(item.getTrnslrName().length() > 0) {
+			writer = String.format("%s/%s", item.getAuthrName().replace("|", ","), item.getTrnslrName().replace("|", ","));
+		} else {
+			writer = item.getAuthrName();
+		}
+		
+		if(item.getLendPsbCdt() == 0) {
+			lendCdt = "대여가능";
+		} else if(item.getLendPsbCdt() == 1){
+			lendCdt = "대여중";
+		} else {
+			lendCdt = "대여불가능";
+		}
+		
+		return new Object[] {
+				item.getBookCode(),
+				item.getBookName().replace("|", ","),
+				writer,
+				String.format("%s[%02d]", item.getPls().getPlsName(), item.getPls().getPlsNo()),
+				String.format("%tF", item.getPblicteYear()),
+				item.getBookPrice(),
+				item.getBookCnt(),
+				lendCdt,
+				item.getTotalLeCnt(),
+				imgCdt,
+				String.format("%s[%02d] / %s[%02d]", item.getLcNo().getLclasName(), item.getLcNo().getLclasNo(), item.getMlNo().getMlsfcName(), item.getMlNo().getMlsfcNo()),
+				String.format("%tF", item.getRegistDate()),
+				dsuseCdt
+		};
+	}
+
+	@Override
+	public void updateRow(Book item, int updateIdx) {
+		String writer;
+		String lendCdt;
+		String imgCdt = item.getBookImg().length != 8706 && item.getBookImg().length > 0? "있음" : "없음";
+		String dsuseCdt = item.getDsuseCdt() == 0 ? "No" : "Yes";
+		
+		if(item.getTrnslrName().length() > 0) {
+			writer = String.format("%s/%s", item.getAuthrName().replace("|", ","), item.getTrnslrName().replace("|", ","));
+		} else {
+			writer = item.getAuthrName();
+		}
+		
+		if(item.getLendPsbCdt() == 0) {
+			lendCdt = "대여가능";
+		} else if(item.getLendPsbCdt() == 1){
+			lendCdt = "대여중";
+		} else {
+			lendCdt = "대여불가능";
+		}
+		
+		model.setValueAt(item.getBookCode(), updateIdx, 0);
+		model.setValueAt(item.getBookName().replace("|", ","), updateIdx, 1);
+		model.setValueAt(writer, updateIdx, 2);
+		model.setValueAt(String.format("%s[%02d]", item.getPls().getPlsName(), item.getPls().getPlsNo()), updateIdx, 3);
+		model.setValueAt(String.format("%tF", item.getPblicteYear()), updateIdx, 4);
+		model.setValueAt(item.getBookPrice(), updateIdx, 5);
+		model.setValueAt(item.getBookCnt(), updateIdx, 6);
+		model.setValueAt(lendCdt, updateIdx, 7);
+		model.setValueAt(item.getTotalLeCnt(), updateIdx, 8);
+		model.setValueAt(imgCdt, updateIdx, 9);
+		model.setValueAt(String.format("%s[%02d] / %s[%02d]", item.getLcNo().getLclasName(), item.getLcNo().getLclasNo(), item.getMlNo().getMlsfcNo(), item.getMlNo().getMlsfcNo()), updateIdx, 10);
+		model.setValueAt(String.format("%tF", item.getRegistDate()), updateIdx, 11);
+		model.setValueAt(dsuseCdt, updateIdx, 12);
+	}
+
+}
+

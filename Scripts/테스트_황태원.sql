1e31634d185b28d55479314f149ebb7fd10bbb7f
bus503 <bus503@daum.net>
2020-03-11 13:27:45 +0900

    memberDaoTest 기능구현 이미지 insert 되는지 확인

--- Scripts/테스트_황태원.sql
+++ Scripts/테스트_황태원.sql
@@ -30,8 +30,6 @@ set lb_id =''
 insert into librarian values
 ('hwangbug@book.ff.kr', 'bug', '황충', '1920-10-12', 42457, '대구광역시 북구 태전로', null, '010-4432-4141', null, 0, '2020-02-10', 0);
 
-select *
-from member;
 
 select mber_id, mber_pass, mber_name, mber_brthdy, mber_zip, mber_bass_ad, mber_detail_ad, mber_tel, mber_img, total_le_cnt, lend_book_cnt, grade, lend_psb_cdt, join_dt, wdr_cdt from member where mber_id = 'ddongtree@naver.com';
 
@@ -39,6 +37,18 @@ updete member
 set 
 where mber_id;
 
+select *
+from member;
+
+select *
+from title;
+
+select *
+from grade;
+
+delete
+from `member`
+where mber_id = 'bus503@daum.net';
 
 
 
--- src/test/java/yi_java3st_3team/dao/MemberDaoTest.java
+++ src/test/java/yi_java3st_3team/dao/MemberDaoTest.java
@@ -2,7 +2,15 @@ package yi_java3st_3team.dao;
 
 import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -11,8 +19,13 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import com.mysql.jdbc.log.Log;
+
 import yi_java3st_3team.dao.impl.MemberDaoImpl;
+import yi_java3st_3team.dto.Grade;
+import yi_java3st_3team.dto.Librarian;
 import yi_java3st_3team.dto.Member;
+import yi_java3st_3team.dto.ZipCode;
 import yi_java3st_3team.util.LogUtil;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -32,49 +45,101 @@ public class MemberDaoTest {
 	}
 
 	@Test
-	public void testSelectMemberByNo() {
-//		LogUtil.prnLog(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
-//		Member member = dao.selectMemberByNo(new Member(""));
-//		if (member.getPic() != null){
-//			getImageToPic(emp.getPic(), emp.getEmpNo());// 프로젝트 폴더의 pics폴더에 사원번호.jpg파일이 생성
-//		}
-//		Assert.assertNotNull(emp);
-//		LogUtil.prnLog(emp);
-//		
-//		//Date를 Calendar 로 변환하여 년/월/일 시/분/초 추출하기
-//		LogUtil.prnLog(String.format("%1$tF - %1$tT %1$tY년 %1$tm월 %1$td일 %1$tH시 %1$tM분 %1$tS초 ", emp.getHireDate()));
-//		
-//		Calendar hire_date = Calendar.getInstance();
-//		hire_date.setTime(emp.getHireDate());
+	public void test01SelectMemberByNo() {
+		LogUtil.prnLog(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Member member = dao.selectMemberByNo(new Member("ssdfr@daum.net"));
+		if(member.getMberImg() != null) {
+			getImageToPic(member.getMberImg(),member.getMberId()); //프로젝트 폴더의 pics 폴더에 사원아이디.jpg 파일이 생성된다
+		}
+		Assert.assertNotNull(member);
+		LogUtil.prnLog(String.format("%1$tF - %1tT %1$tY년 %1$tm월 %1$td일 ", member.getJoinDt()));
+		LogUtil.prnLog(String.format("%1$tF - %1tT %1$tY년 %1$tm월 %1$td일 ", member.getMberBrthdy()));
 		
-//		LogUtil.prnLog(String.format("%d 년", hire_date.get(Calendar.YEAR)));
-//		LogUtil.prnLog(String.format("%d 월", hire_date.get(Calendar.MONTH) + 1));
-//		LogUtil.prnLog(String.format("%d 일", hire_date.get(Calendar.DAY_OF_MONTH)));
-//		LogUtil.prnLog(String.format("%d시 %d분 %d초", hire_date.get(Calendar.HOUR_OF_DAY), hire_date.get(Calendar.MINUTE), hire_date.get(Calendar.SECOND)));
+		Calendar joinDate = Calendar.getInstance();
+		Calendar birthday = Calendar.getInstance();
+		joinDate.setTime(member.getJoinDt());
+		birthday.setTime(member.getMberBrthdy());
+		
+		LogUtil.prnLog(String.format("%d 년", joinDate.get(Calendar.YEAR)));
+		LogUtil.prnLog(String.format("%d 월", joinDate.get(Calendar.MONTH)+1));
+		LogUtil.prnLog(String.format("%d 일", joinDate.get(Calendar.DAY_OF_MONTH)));
+		
+		LogUtil.prnLog(String.format("%d 년", birthday.get(Calendar.YEAR)));
+		LogUtil.prnLog(String.format("%d 월", birthday.get(Calendar.MONTH)+1));
+		LogUtil.prnLog(String.format("%d 일", birthday.get(Calendar.DAY_OF_MONTH)));
+	}
+
+	
+
+	@Test
+	public void test02SelectMemberByAll() {
+		LogUtil.prnLog(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		List<Member> list = dao.selectMemberByAll();
+		Assert.assertNotEquals(0, list.size());
+		LogUtil.prnLog(list);
 	}
 
 	@Test
-	public void testSelectMemberByAll() {
+	public void test03InsertMember() {
 		LogUtil.prnLog(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
-		fail("Not yet implemented");
+		Calendar c= Calendar.getInstance();
+		Date birthday = new Date(c.getTimeInMillis());
+		Date joinDate = new Date(c.getTimeInMillis());
+		
+		Member member = new Member("bus503@daum.net", "xodnjs", "황태원", birthday, new ZipCode(42457), "대구광역시 북구 태전로", "루존 202호", "010-4245-3825", 100, 2, new Grade(1), 1, joinDate, 1);
+		member.setMberImg(getImage("lyy.jpg"));
+		LogUtil.prnLog(member);
+		int res = dao.insertMember(member);
+		Assert.assertEquals(1, res);
 	}
 
 	@Test
-	public void testInsertMember() {
+	public void test04UpdateMember() {
 		LogUtil.prnLog(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
-		fail("Not yet implemented");
+		Member member = new Member("bus503@daum.net", "xodnjs", "장수황씨", new Date(), new ZipCode(42457), "대구광역시 북구 태전로", "루존 202호", "010-4245-3825", 100, 2, new Grade(1), 1, new Date(), 1);
+		int res = dao.updateMember(member);
+		Assert.assertEquals(1, res);
+		LogUtil.prnLog(member);
 	}
 
 	@Test
-	public void testUpdateMember() {
+	public void test05DeleteMember() {
 		LogUtil.prnLog(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
-		fail("Not yet implemented");
+		Member member = new Member("bus503@daum.net");
+		int res = dao.deleteMember(member);
+		Assert.assertEquals(1, res);
+		LogUtil.prnLog(member);
+	}
+	
+	private byte[] getImage(String ImgName) {
+		byte[] image = null;
+		File file = new File(System.getProperty("user.dir")+File.separator+"images",ImgName);
+		try(InputStream is = new FileInputStream(file)){
+			image = new byte[is.available()];
+			is.read(image);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return image;
+	}
+	
+	private void getImageToPic(byte[] mberImg, String mberId) {
+		File picsDir = new File(System.getProperty("user.dir") + File.separator + "pics");
+		if (!picsDir.exists()) {
+			picsDir.mkdir();
+		}
+		
+		File file = new File(picsDir, mberId +".jpg");
+		try(FileOutputStream save = new FileOutputStream(file)){
+			save.write(mberImg);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	@Test
-	public void testDeleteMember() {
-		LogUtil.prnLog(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
-		fail("Not yet implemented");
-	}
 
 }

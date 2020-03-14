d4357e07e50be5c8a20e42fb3a7d9bac9da76bb8
hotdog1247 <ku0788@yahoo.com>
2020-03-14 19:13:11 +0900

    대여도서 권수 수정

--- Scripts/테스트_이상원_대여반납.sql
+++ Scripts/테스트_이상원_대여반납.sql
@@ -38,6 +38,13 @@ select mber_id, mber_pass, mber_name, mber_brthdy, mber_zip,mber_bass_ad,mber_de
 	from member
 	where mber_id = 'daddystop@gmail.com';
 
+select *
+	from lending
+	where rturn_date is null;
+
+select *
+	from lending;
+
 select book_code ,book_name ,authr_name ,trnslr_name , pls, pblicte_year ,book_price ,lend_psb_cdt ,total_le_cnt ,book_img , lc_no , ml_no , regist_date , dsuse_cdt 
 	from book
 	where book_code = 'A090101';
